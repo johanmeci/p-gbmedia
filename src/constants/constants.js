d5e983e854a52a28068e035b3bc6f86fb218bc04
Johan Meneses <johanmeci03@gmail.com>
2022-01-13 21:18:45 -0500

    Adding items about us section

---
+++ src/constants/constants.js
@@ -0,0 +1,58 @@
+export const itemsAbout = [
+	{
+		title: 'No prometemos lo que no podemos hacer',
+		category: 'calidad',
+		image: '/images/iconCalidad.svg',
+		description: 'Si creemos que algo no lo podemos hacer, por la razón que sea, se lo diremos y buscaremos una alternativa.',
+		id: 0
+	},
+	{
+		title: 'Contamos con profesionales calificados',
+		category: 'servicio',
+		image: '/images/iconAsesoramiento.svg',
+		description: 'No somos desarrolladores que hacen un poco de diseño o diseñadores que hacen un poco de desarrollo. Somos una de las pocas empresas que pueden fusionar tecnología y creatividad, y eso se ve reflejado en nuestros trabajos.',
+		id: 1
+	},
+	{
+		title: 'Vamos más allá',
+		category: 'servicio',
+		image: '/images/iconAsesoramiento.svg',
+		description: 'Buscamos la excelencia. Nuestros trabajos y negocios son muy importantes para nosotros, por lo tanto, todo lo que hacemos por nuestros clientes es muy importante. No dudamos en considerar su éxito como nuestro, por lo tanto, haremos todo lo posible para que usted desarrolle negocios exitosos con nuestra colaboración.',
+		id: 2
+	},
+	{
+		title: 'Tenemos experiencia en distintas industrias',
+		category: 'calidad',
+		image: '/images/iconCalidad.svg',
+		description: 'Nuestro trabajo abarca tanto la industria IT, organizaciones, viajes, enseñanza, transporte, moda, eventos, portales verticales, sofisticadas plataformas y otros, interactuando en cada proyecto, colaborando y aprendiendo cada día más.',
+		id: 3
+	},
+	{
+		title: 'Estamos concentrados 100% en la calidad',
+		category: 'profesionalismo',
+		image: '/images/iconProfesionalismo.svg',
+		description: 'La calidad es nuestro compromiso con las empresas que confían en nosotros. En GB Media Group evaluamos todo, son sometidos a una constante evaluación nuestros procesos, empleados, desarrollos, diseños, la documentación, tests y comunicación.',
+		id: 4
+	},
+	{
+		title: 'No nos limitamos a ninguna tecnología',
+		category: 'innovación',
+		image: '/images/iconInnovacion.svg',
+		description: 'Debido a nuestro amplio y profundo nivel de conocimientos en distintos lenguajes, tenemos la capacidad de evaluar su proyecto y proponerle una plataforma tecnológica que se ajuste perfectamente a sus necesidades. Desarrollamos soluciones en .NET, PHP, C++, Java (J2EE), SOA (Arquitectura Orientada a Servicios), XML, JQuery, Javascript, por nombrar sólo algunas.',
+		id: 5
+	},
+	{
+		title: 'Elaboramos una sólida documentación',
+		category: 'servicio',
+		image: '/images/iconAsesoramiento.svg',
+		description: 'En los casos que el desarrollo lo requiera, creamos documentación muy detallada y completa.',
+		id: 6
+	},
+	{
+		title: 'Nuestra gestión se involucra en todas las etapas del proyecto',
+		category: 'profesionalismo',
+		image: '/images/iconProfesionalismo.svg',
+		description: 'Nuestra gestión es detallista y calificada, participando y sumando valor en las distintas etapas de desarrollo del proyecto.',
+		id: 7
+	}
+]
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,38 @@
+import { Accordion, Container, Row } from "react-bootstrap";
+
+import { itemsAbout } from "../constants/constants";
+
+import '../css/homePage.css';
+
 export default function HomePage() {
   return (
     <main className="serviciosIndex" id="serviciosIndex">
-      <h3>Txto</h3>
+      <Container>
+        <Row>
+          <section className="contenedorServicios col-12 col-md-11 col-lg-12">
+            <h3>¿Por qué elegirnos?</h3>
+            <h6>Nuestra agencia cuenta con una estructura multidisciplinaria que nos permite la especialización en distintas áreas; promoviendo un intercambio ideológico constante que aporte valor agregado a nuestro proceso creativo, desarrollando conceptos y estrategias impactantes y efectivas.</h6>
+            <Accordion className="contenedorServicios col-12 col-md-11 col-lg-12">
+            {itemsAbout.map(({ title, category, description, image, id }) => (
+
+              <Accordion.Item eventKey={id} className="servicio" key={id}>
+                <Accordion.Header className="headerServicio">
+                  <img src={image} alt="icon {category}" className="iconServicio"/>
+                  <div className="divHeader">
+                    <h5 className="txtServicio">{category}</h5>
+                    <h4 className="tituloServicio">{title}</h4>
+                  </div>
+                </Accordion.Header>
+                <Accordion.Body className="bodyServicio">
+                  <p className="textoServicio">{description}</p>
+                </Accordion.Body>
+              </Accordion.Item>
+
+            ))}
+            </Accordion>
+          </section>
+        </Row>
+      </Container>
     </main>
   )
 }
