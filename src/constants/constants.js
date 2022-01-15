d5869b9c8a416ded478ef11be83350664c521a2c
Johan Meneses <johanmeci03@gmail.com>
2022-01-15 16:58:53 -0500

    Adding services page

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -79,3 +79,169 @@ export const txtHero = [
     subtitle: ''
   }
 ]
+
+export const contentServices = [
+  {
+    id: 0,
+    tag: 'web',
+    title: 'Servicio Web',
+    items: [
+      {
+        id: 0,
+        txt: 'Desarrollo Web',
+        description: 'Con nosotros tienes la oportunidad de elegir un plan de diseño que se ajuste a sus necesidades, así, nos encargaremos de interpretar sus requerimientos y los de su negocio, para enfocarnos en diseñar una manera sencilla de comunicar el mensaje de su marca.'
+      },
+      {
+        id: 1,
+        txt: 'Aplicativos Web',
+        description: 'Evaluamos lo que usted pueda necesitar y diseñamos una herramienta informática para su entidad que puede realizar las mismas funciones que aplicaciones de escritorio, con la ventaja de ser accesible desde internet.'
+      },
+      {
+        id: 2,
+        txt: 'SEO',
+        description: 'El objetivo de nuestra estrategia es asegurarnos que el sitio es optimizado para ser más relevante a palabras clave directamente relacionadas con la industria, productos y/o servicios de su empresa.'
+      }
+    ],
+    images: [
+      {
+        id: 0,
+        type: 'svg',
+        image: '/images/imgSeccionServicios.svg'
+      },
+      {
+        id: 1,
+        type: 'svg',
+        image: '/images/imgSeccionServicios2.svg'
+      }
+    ]
+  },
+  {
+    id: 1,
+    tag: 'audiovisual',
+    title: 'Producción Audiovisual',
+    items: [
+      {
+        id: 0,
+        txt: 'Instalaciones',
+        description: 'Contamos en nuestras instalaciones con equipos de grabación, estabilización e iluminación de última tecnología y un ciclorama que cuenta con todas las capacidades técnicas para producciones video y fotografía profesionales.'
+      },
+      {
+        id: 1,
+        txt: 'Innovadoras Técnicas',
+        description: 'Utilizamos la fotografía de Alto Rango Dinámico (HDR) muy interesante en la fotografía de arquitectura e interiorismo pues permite equilibrar las diferencias de luz, o los Timelapses que pueden ser un complemento ideal al poder ver la evolución de un día.'
+      },
+      {
+        id: 2,
+        txt: 'Servicio de Calidad',
+        description: 'La meta, el fin de nuestros servicios imágenes originales, atractivas, impactantes, sugerentes, que transmiten un mensaje y venden una idea, producto o servicio. Ofrecemos trabajos, con una excelente relación calidad/precio. No se trata de un gasto, sino de una inversión muy rentable.'
+      }
+    ],
+    images: [
+      {
+        id: 0,
+        type: 'jpg',
+        image: '/images/imgAudiovisual1.jpg'
+      },
+      {
+        id: 1,
+        type: 'jpg',
+        image: '/images/imgAudiovisual2.jpg'
+      }
+    ]
+  },
+  {
+    id: 2,
+    tag: 'design',
+    title: 'Imagen Corporativa',
+    items: [
+      {
+        id: 0,
+        txt: 'Logotipos',
+        description: 'Creamos diseños de logotipos para empresas fuera de lo común, actualizando y mejorando marcas ya existentes. El proceso de diseño de logo requiere una idea clara del concepto y valores de la marca, además de una comprensión del consumidor de la audiencia meta.'
+      },
+      {
+        id: 1,
+        txt: 'Branding',
+        description: 'Dentro de nuestro equipo de trabajo interdisciplinar contamos con diseñadores gráficos especializados en el desarrollo de proyectos de branding, diseño audiovisual, editoriales e impresos.'
+      },
+      {
+        id: 2,
+        txt: 'Arquitectura Corporativa',
+        description: 'Diseñamos y cuidamos cualquier objeto o imagen que represente a la empresa u organización consiguiendo que las empresas generen más confianza obteniendo una identidad corporativa con una coherencia y uniformidad que será percibida por el público de una manera mucho más receptiva.'
+      }
+    ],
+    images: [
+      {
+        id: 0,
+        type: 'jpg',
+        image: '/images/imgDesign1.jpg'
+      },
+      {
+        id: 1,
+        type: 'jpg',
+        image: '/images/imgDesign2.jpg'
+      }
+    ]
+  }
+]
+
+export const contentStages = [
+  {
+    id: 0,
+    title: 'Etapa de Atracción',
+    description: 'La finalidad de esta primera etapa es simple, se trata de generar contenido valioso y relevante acerca del mercado al que pertenece tu empresa, despertando la atención de potenciales clientes y que espontáneamente, se interesen en las publicaciones de tu contenido y por ende, en tus productos y servicios.'
+  },
+  {
+    id: 1,
+    title: 'Etapa de Conversión',
+    description: 'En esta segunda etapa, comenzamos a captar la información de nuestros visitantes para luego perfilarlos y proyectarnos al público objetivo. Es importante brindar contenido de valor al cliente ideal.'
+  },
+  {
+    id: 2,
+    title: 'Etapa de Nutrición',
+    description: 'Sabemos que todo usuario busca servicios o productos en la Web, pero antes de comprar necesitan saber si lo que buscan puede solucionarles su problema antes de realizar la compra, por esa razón, se debe crear contenido de valor que incentive la compra. En esta etapa comenzamos a nutrir con contenido de valor a tus prospectos. En esta etapa nuestro enfoque es automatizar la relación con ellos mientras es dirigido por un embudo enfocado en ventas.'
+  },
+  {
+    id: 3,
+    title: 'Etapa de Venta',
+    description: 'Todo el trabajo de generación de Leads y relacionamiento tiene como objetivo producir demanda y generar oportunidades de negocio para su empresa. En algunos casos, el proceso de venta ocurre dentro del propio sitio web; en otros, es necesario el contacto entre un vendedor y el cliente potencial.'
+  },
+  {
+    id: 4,
+    title: 'Etapa de Análisis',
+    description: 'Uno de los principales diferenciales del Marketing Digital es poder evaluar con precisión sus resultados y comprobar el retorno de la inversión con base en los hechos. Es posible monitorear cada acción que ocurre en las interacciones de su público con su sitio web, posts en redes sociales y campañas pagas, lo que hace un trabajo más inteligente. Por otra parte, todo el proceso puede ser optimizados mediante la detección de las tasas que están debajo de lo esperado para direccionar los esfuerzos del equipo de marketing.'
+  }
+]
+
+export const contentCallCenter = [
+  {
+    id: 0,
+    title: 'Atención personalizada',
+    description: 'Gracias a contratar un servicio de call center se puede ofrecer un servicio personalizado a cada cliente.'
+  },
+  {
+    id: 1,
+    title: 'Ahorro económico',
+    description: 'El disponer de un call center en las mismas instalaciones que la empresa supone un gasto importante. Externalizar este servicio es sinónimo de ahorro de costes.'
+  },
+  {
+    id: 2,
+    title: 'Campañas',
+    description: 'Alquilar un call center por campañas o temporadas comerciales concretas, ayuda al aumento de ventas de cualquier empresa. Y hoy en día, es un factor a tener en cuenta.'
+  },
+  {
+    id: 3,
+    title: 'Valor añadido',
+    description: 'Que una organización o una institución disponga de un call center ofrece un calor añadido a la empresa ya que se preocupa por mantener un contacto directo con el cliente final.'
+  },
+  {
+    id: 4,
+    title: 'Soporte a los usuarios',
+    description: 'Si la empresa en cuestión se dedica a la venta de diferentes productos, puede ofrecer mediante esta solución una atención y un soporte a sus clientes.'
+  },
+  {
+    id: 5,
+    title: 'Servicio de llamadas entrantes',
+    description: 'Como, por ejemplo, la petición de citas en caso de negocios o instituciones que requieran de este servicio para una mejor gestión y organización de su trabajo.'
+  }
+]
--- src/pages/ServicesPage.js
+++ src/pages/ServicesPage.js
@@ -1,5 +1,96 @@
+import { Accordion, Container, Row } from "react-bootstrap";
+import { contentCallCenter, contentServices, contentStages } from "../constants/constants";
+
+import '../css/servicesPage.css';
+
 export default function Servicios() {
-    return (
-        <h1>Servicios</h1>
-    )
+  return (
+    <main className="serviciosPrincipal" id="serviciosPrincipal">
+      <Container>
+        <Row>
+          {contentServices.map(({ id, tag, title, items, images }) => (
+            
+          <section className={`contenedorServicio ${tag} col-12`} id={`${tag}`} key={id}>
+
+            <div className={`contenedorDisplay ${images[0].type}`}>
+              <h3>{title}</h3>
+              <div className="contenedorImg">
+              {images.map(({ id, type, image }) => (
+
+                type === 'svg' ? <img src={image} alt="img servicio" key={id}/> : <div className={`img${id+1}`} style={{backgroundImage: `url(${image})`}} key={id}></div>
+                
+              ))}
+              </div>
+            </div>
+            <div className="contenedorTexto">
+              {items.map(({ id, txt, description }) => (
+                <div className="divTexto" key={id}>
+                  <h6>{txt}</h6>
+                  <p>{description}</p>
+                </div>
+              ))}
+            </div>
+            
+          </section>
+          
+          ))}
+
+          <section className="contenedorServicio posicionamiento col-12" id="posicionamiento">
+            <div className="contenedorTexto">
+              <h3>Estrategias de posicionamiento de marca</h3>
+              <p>En GB Media Group te ayudamos a transformar tu manera de atraer y captar clientes a través de internet. Construimos comunidad alrededor de tu marca y la fidelizamos a través de contenidos y automatización. Le convertimos en una autoridad en su sector; nosotros solo hablamos de resultados.</p>
+            </div>
+            <div className="contenedorEtapas">
+              <h4>Dividimos nuestro proceso en cinco etapas</h4>
+              <Accordion className="contenedorMaster">
+                {contentStages.map(({ id, title, description }) => (
+                  <Accordion.Item eventKey={id} className="etapa">
+                    <Accordion.Header className="headerEtapa">
+                      <h5>{title}</h5>
+                    </Accordion.Header>
+                    <Accordion.Body className="bodyEtapa" id={`bodyEtapa${id+1}`}>
+                      <p>{description}</p>
+                    </Accordion.Body>
+                  </Accordion.Item>
+                ))}
+              </Accordion>
+            </div>
+          </section>
+
+          <section className="contenedorServicio callcenter col-12" id="callcenter">
+            <div className="contenedorTexto">
+              <h3>Call Center</h3>
+              <p>Ofrecemos una solución completa de callcenter para compañías de todos los tamaños, nuestro servicio es escalable y se ajusta a las necesidades de tanto empresas en crecimiento como grandes compañías; estas son las razones por la que lo invitamos a contratar nuestro servicio de callcenter.</p>
+              <div className="imgSeccionCall"></div>
+            </div>
+            <div className="contenedorLista">
+              {contentCallCenter.map(({ id, title, description }) => (
+                <div className="lista" key={id}>
+                  <h5>{title}</h5>
+                  <p>{description}</p>
+                </div>
+              ))}
+            </div>
+          </section>
+
+          <section className="contenedorServicio hosting col-12" id="hosting">
+            <h3>Hosting y Data Center</h3>
+            <div className="contenedorTexto">
+              <div className="divTexto">
+                <h6>Hosting</h6>
+                <p>Nuestro departamento de programación desarrolla soluciones a la medida según las necesidades específicas de clientes en diversos sectores.</p>
+              </div>
+              <div className="divTexto">
+                <h6>Data Center</h6>
+                <p>Diseñado para cubrir las necesidades técnicas de los entornos TIC empresariales, los servicios de data center de GB media group son la mejor alternativa para los clientes que quieren centrar sus recursos en áreas que aportan mayor valor a su negocio.</p>
+              </div>
+            </div>
+            <div className="divDisplay">
+              <div className="imgDataCenter"></div>
+            </div>
+          </section>
+        </Row>
+      </Container>
+    </main>
+  )
 }
