dd2f6fb9b30e7600185dbca9f579e47b370239a7
Johan Meneses <johanmeci03@gmail.com>
2022-01-25 17:49:25 -0500

    Adding scss to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,6 @@
 import { Col, Container, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import './footer.css';
+import './footer.scss';
 
 export default function Footer() {
   return (
